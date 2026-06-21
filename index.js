0d0161aca3d5ed3ff98168ba24ca41de338e02e9
bistpramod <bistpramod113@gmail.com>
2026-06-21 16:47:19 +0545

    start of backemd

---
+++ index.js
@@ -0,0 +1,36 @@
+// // module (import)
+// const add = require("./math.js")
+
+// console.log(add);
+
+
+// // console.log(math.add(13,2))
+
+
+//! types of modules
+// 1 , local module 
+// 2 , core module 
+// 3, third party modules 
+
+
+//! Local Modules 
+
+//  a , commonJS module -- > defailt 
+// const math = require , tpe 
+
+
+
+
+
+
+//b) esm import 
+// default and named both 
+// in default we can import by == import anyVariable from ''../path'' anyVariable is anything thatwe can write there is no strict rule for it ?
+// but we cannot use 2 default exports at once and we have to do the named export 
+
+// eg import  {namedExport}  from "./path "
+
+
+
+// npm 
+// node package manager 
---
+++ math.js
@@ -0,0 +1,20 @@
+// const add = (a,b) =>{
+//     return a+b;
+
+// };
+
+// const sub = (a,b) =>{
+//     return a-b;
+// };
+
+// // module.exports = add;
+// module.export ={
+//     add,
+//     sub,
+// }
+
+// or there is another  method too 
+exports.add= (a,b) =>{
+    return a+b;
+
+}
